dc7aab1b3f2370778a8b84c38847ddeeb9798575
Niklas Voss <niklas.voss@gmail.com>
2023-09-15 14:37:17 +0200

    Basic nix flake devenv

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "gke-ap-asm-hpa-demo";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    let
+      overlays = [
+      ];
+    in
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      rec {
+        devShell = pkgs.mkShell rec {
+          name = "gke-ap-asm-hpa-demo";
+
+          buildInputs = with pkgs; [
+            gnumake
+            findutils
+            tmux
+            google-cloud-sdk
+            (google-cloud-sdk.withExtraComponents [
+              pkgs.google-cloud-sdk.components.kubectl
+              pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin
+            ])
+            terraform
+            tfsec
+            tflint
+            pulumi-bin
+          ];
+        };
+      }
+    );
+}
